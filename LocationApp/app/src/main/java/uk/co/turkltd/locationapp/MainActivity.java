319318679befbcaa5fb8d7acd60f4ed3e3dd37bd
BTSTURKEY <paxt24@gmail.com>
2020-03-06 15:16:09 +0000

    Save, Encrypt and Display URL

--- LocationApp/app/src/main/java/uk/co/turkltd/locationapp/MainActivity.java
+++ LocationApp/app/src/main/java/uk/co/turkltd/locationapp/MainActivity.java
@@ -1,14 +1,82 @@
 package uk.co.turkltd.locationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.security.crypto.MasterKeys;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class MainActivity extends AppCompatActivity {
+
+    String masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+
+    EncryptedSharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    String serverURL, savedURL;
+    EditText serverInput;
+    TextView displayURL;
+    Button submitButton;
+    Button locationButton;
+
+    public MainActivity() throws GeneralSecurityException, IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            sharedPreferences = (EncryptedSharedPreferences) EncryptedSharedPreferences.create(
+                    "serverURL",
+                    masterKeyAlias,
+                    getApplicationContext(),
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+
+            editor = sharedPreferences.edit();
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
+
+        savedURL = sharedPreferences.getString("URL", "");
+
+        displayURL = findViewById(R.id.displayURL);
+        displayURL.setText(savedURL);
+
+
+        serverInput = findViewById(R.id.serverInput);
+
+        submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                serverURL = serverInput.getText().toString();
+
+                editor.putString("URL", serverURL);
+                editor.apply();
+
+                savedURL = sharedPreferences.getString("URL", "");
+                displayURL = findViewById(R.id.displayURL);
+                displayURL.setText(savedURL);
+            }
+        });
+
+        locationButton = findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
