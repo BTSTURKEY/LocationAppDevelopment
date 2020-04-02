9c3ab4a27ed1611f181733cff82980ade9b535a4
BTSTURKEY <paxt24@gmail.com>
2020-04-02 16:16:21 +0100

    Location tracking implemented

--- LocationApp/app/src/main/java/uk/co/turkltd/locationapp/MainActivity.java
+++ LocationApp/app/src/main/java/uk/co/turkltd/locationapp/MainActivity.java
@@ -2,12 +2,23 @@ package uk.co.turkltd.locationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.security.crypto.MasterKeys;
 import androidx.security.crypto.EncryptedSharedPreferences;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.Criteria;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -20,33 +31,36 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Encryption key
-    String masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
-
-    //
+    //Objects
     EncryptedSharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    LocationManager locationManager;
 
     //Variables
-    String serverURL, savedURL;
+    String masterKeyAlias, serverURL, savedURL;
     boolean switchState, buttonState;
+    double longitude, latitude;
 
     //Widgets
     EditText serverInput;
-    TextView displayURL;
+    TextView displayURL, longitudeValue, latitudeValue;
     Button submitButton;
     ToggleButton locationButton;
     Switch consentSwitch;
 
-    public MainActivity() throws GeneralSecurityException, IOException {
+
+    public MainActivity() {
+        try {
+            masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         try {
             //Encrypt data being sent to shared preferences, located in data/data/uk.co.turkltd.locationapp/shared_prefs/serverURL.xml
             sharedPreferences = (EncryptedSharedPreferences) EncryptedSharedPreferences.create(
@@ -57,63 +71,41 @@ public class MainActivity extends AppCompatActivity {
                     EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
             );
 
-            editor = sharedPreferences.edit();
 
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
         }
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        editor = sharedPreferences.edit();
+        editor.apply();
+
         displayURL = findViewById(R.id.displayURL);
         submitButton = findViewById(R.id.submitButton);
         serverInput = findViewById(R.id.serverInput);
         consentSwitch = findViewById(R.id.consentSwitch);
+        longitudeValue = findViewById(R.id.longitudeValue);
+        latitudeValue = findViewById(R.id.latitudeValue);
         locationButton = findViewById(R.id.locationButton);
 
-        //Set displayURL to what is saved in shared preferences
-        savedURL = sharedPreferences.getString("URL", "");
-        displayURL.setText(savedURL);
+        checkStates();
 
         //Submit button to save server URL to shared preferences as well as display result in displayURL
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                serverURL = serverInput.getText().toString();
-
-                editor.putString("URL", serverURL);
-                editor.apply();
-
-                savedURL = sharedPreferences.getString("URL", "");
-                displayURL = findViewById(R.id.displayURL);
-                displayURL.setText(savedURL);
+            public void onClick(View v) {
+                saveURL();
             }
         });
 
-        //Check state of switch from shared preferences and apply required settings.
-        switchState = sharedPreferences.getBoolean("SWITCH", false);
-        consentSwitch.setChecked(switchState);
-        if (switchState = true){
-            locationButton.setVisibility(View.VISIBLE);
-        }
-
-        //Check state of toggle button from shared preferences and apply required settings.
-        buttonState = sharedPreferences.getBoolean("BUTTON", false);
-        locationButton.setChecked(buttonState);
-
-        //Switch to give make sure users have to give consent to send location
+        //Switch to make sure users have to give consent to send location
         consentSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                    locationButton.setVisibility(View.VISIBLE);
-                    editor.putBoolean("SWITCH", true);
-                    editor.apply();
-
+                if (isChecked) {
+                    consentOn();
                 } else {
-                   locationButton.setVisibility(View.INVISIBLE);
-                   locationButton.setChecked(false);
-                   editor.putBoolean("SWITCH", false);
-                   editor.apply();
+                    consentOff();
                 }
             }
         });
@@ -122,20 +114,137 @@ public class MainActivity extends AppCompatActivity {
         locationButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-
-
-                    editor.putBoolean("BUTTON", true);
-                    editor.apply();
-
-
-
+                if (isChecked) {
+                    sendLocation();
                 } else {
-                    editor.putBoolean("BUTTON", false);
-                    editor.apply();
-
+                    noSendLocation();
                 }
             }
         });
+    }
+
+
+    public void checkStates() {
+        //Set displayURL to what is saved in shared preferences
+        savedURL = sharedPreferences.getString("URL", "");
+        displayURL.setText(savedURL);
+
+        //Check state of switch from shared preferences and apply required settings.
+        switchState = sharedPreferences.getBoolean("SWITCH", false);
+        consentSwitch.setChecked(switchState);
+        if (switchState = true) {
+            locationButton.setVisibility(View.VISIBLE);
+        }
+
+        //Check state of toggle button from shared preferences and apply required settings.
+        buttonState = sharedPreferences.getBoolean("BUTTON", false);
+        locationButton.setChecked(buttonState);
+    }
+
+    public void saveURL() {
+        serverURL = serverInput.getText().toString();
+        editor.putString("URL", serverURL);
+        editor.apply();
+        savedURL = sharedPreferences.getString("URL", "");
+        displayURL = findViewById(R.id.displayURL);
+        displayURL.setText(savedURL);
+    }
+
+    public void alertUser() {
+        new AlertDialog.Builder(this)
+                .setTitle("Location settings")
+                .setMessage("Location services are deactivated, please enable them to use the application")
+                .setPositiveButton("Location settings", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        dialog.dismiss();
+                        startActivity(i);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    public void consentOn() {
+        boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean network = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if (!gps || !network) {
+            alertUser();
+            consentSwitch.setChecked(false);
+        } else if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+            consentSwitch.setChecked(false);
+        } else {
+            locationButton.setVisibility(View.VISIBLE);
+            editor.putBoolean("SWITCH", true);
+            editor.apply();
+        }
+    }
+
+    public void consentOff() {
+        locationButton.setVisibility(View.INVISIBLE);
+        locationButton.setChecked(false);
+        editor.putBoolean("SWITCH", false);
+        editor.putBoolean("BUTTON", false);
+        editor.apply();
+    }
+
+    private void sendLocation() {
+        Log.v("method", "1");
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(criteria, true);
+        Log.v("method", "2");
+        if (provider != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(provider, 0, 0, receiveLocation);
+                editor.putBoolean("BUTTON", true);
+                editor.apply();
+                Log.v("method", "3");
+            }
+        }
+    }
+
+    private final LocationListener receiveLocation = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.v("method", "4");
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            Log.v("method", "5");
+            String lat = Double.toString(latitude);
+            String lon = Double.toString(longitude);
+            Log.v("method", "6");
+            latitudeValue.setText(lat);
+            longitudeValue.setText(lon);
+            Log.v("method", "7");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void noSendLocation() {
+        editor.putBoolean("BUTTON", false);
+        editor.apply();
     }
 }
